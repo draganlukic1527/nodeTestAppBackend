96da9994869cb823148d74af5d046ba416010cae
draganlukic1527 <draganlukic1527@gmail.com>
2023-09-03 01:05:06 -0700

    updated node script and docker files

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,7 @@
 # STAGE: Development
-FROM node:16-alpine AS dev
-
-RUN apk --no-cache add curl
-
-# Port to listen on
-EXPOSE 8002
-
-# Copy app and install packages
-WORKDIR /nodeTestApp
-COPY . /nodeTestApp/
-
-RUN yarn
-
-HEALTHCHECK --interval=10s --timeout=5s --retries=3 \
-  CMD curl --fail http://localhost:8002/api/health-check || kill 1
-
-# Default app commands
-ENTRYPOINT ["sh", "-c", "node index.js"]
+FROM node:lts-alpine
+WORKDIR /app
+COPY . .
+RUN npm install
+EXPOSE 8080
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT,
+});
+connection.connect(function (err) {
+	if (err) console.log(err.message);
+	console.log("Successfully connected");
+});
+
 // Simulated user data (replace this with a database)
 const users = [
 	{ id: 1, username: "user1", password: "password1" },
 	{ id: 2, username: "user2", password: "password2" },
 ];
+
+app.get("/", (req, res) => {
+	res.status(200).send("Hello, World!");
+});
 
 app.post("/api/login", (req, res) => {
 	const { username, password } = req.body;
@@ -27,6 +44,6 @@ app.post("/api/login", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+	console.log(`[INFO] Listening on http://localhost:${PORT}`);
 });
